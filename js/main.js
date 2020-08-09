f3072789d4747ab95f98473fdeee41f3baff756b
arinary5959 <arinary5959@gmail.com>
2020-08-09 15:38:55 +0900

    카카오톡클론 추가 중
    카카오톡클론 추가 중

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const menuBtn = document.querySelector('.main_menu_bar')
-console.log(menuBtn)
+const kakaodesLi = document.querySelector('.clone-web-kakaoapp')
+const modalPopUl = document.querySelector('.clone-web-kakaoapp > div > ul')
+console.log(kakaodesLi)
 
 menuBtn.addEventListener('click', clickWork)
+
+kakaodesLi.addEventListener('click', kakaoDesWork)
+modalPopUl.addEventListener('click', modal)
 
 function clickWork (e){
     console.log(this)
@@ -10,4 +15,17 @@ function clickWork (e){
     }else{
         this.classList.add('on')
     }
+}
+function kakaoDesWork(e){
+    e.preventDefault()
+    console.log(this)
+    console.log(e)
+    if(this.classList.contains('on')){
+        this.classList.remove('on')
+    }else{
+        this.classList.add('on')
+    }
+}
+function modal(e){
+    e.stopPropagation()
 }
